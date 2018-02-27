666616fc7c7a78fe818f5191b481300d0e4a4281
mark.brown8790@gmail.com <mark.brown8790@gmail.com>
2018-02-27 14:44:53 -0500

    Updated tutorial page to bring it up to speed. Added email links but not completely finished as of yet.

--- MAUT_1_1_3/example/views.py
+++ MAUT_1_1_3/example/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-#from .forms import *
-#from .models import UserProfile, Decide, Item, Criteria
 from django.shortcuts import render, redirect, render_to_response
-#from .restaurant_api import *
+from .forms import *
+from django.core.mail import EmailMessage, send_mail, BadHeaderError
+
+
 
 # Create your views here.
 
 def example(request):
-    #if request.method == 'POST':
-        #zipFilterForm = ZipFilterForm(request.POST)
+    if request.method == 'POST':
+        contactForm = ContactForm(request.POST)
+        if contactForm.is_valid():
+            subject = "MAUT Contact Form Submission"
+            message_content = contactForm.cleaned_data['content']
+            if contactForm.cleaned_data['contact_email']:
+                from_email = contactForm.cleaned_data['contact_email']
+            else:
+                from_email = "n/a"
 
-        #if zipFilterForm.is_valid():
-            #request.session['decision'] = RestaurantAPI()
-            #request.session['decision'].locationFilter(zipFilterForm.cleaned_data['zip_code'])
+            message = "***This message is an automatically generated message from MAUT's contact page***\n" + "Name: " + contactForm.cleaned_data['contact_name'] + "\n" + "Email Address: " + from_email + "\n" + "Message: " + message_content + "\n"
 
-            #return HttpResponseRedirect('/restaurants/criteria/')
+            try:
+                send_mail(subject, message, from_email, ['markamay@live.com','MAUtilityTheory@gmail.com','mark.brown8790@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return render(request,'contact/thankyou.html', {})
+        else:
+            return render(request,'contact/contact.html', {'form': ContactForm()})
 
-    #else:
-        #zipFilterForm = ZipFilterForm()
+    else:
+        contactForm = ContactForm()
 
-    return render(request, 'example/index.html')
+    # return render(request,'contact/contact.html', {'form': ContactForm()})
+
+    return render(request, 'example/index.html', {'form': ContactForm()})
