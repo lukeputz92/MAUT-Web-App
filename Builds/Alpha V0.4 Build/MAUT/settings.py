5c823f71c714422f12f21ab69db126974220ec73
Mark <markamay@live.com>
2018-02-27 21:33:37 -0500

    Added pythonanywhere on the list of accepted hosts

--- Builds/Alpha V0.4 Build/MAUT/settings.py
+++ Builds/Alpha V0.4 Build/MAUT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$^@@m)=r)t=qvekpf03&@x7t%y-#a$=k1-tu@*nht-t-@(c=@$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maut.pythonanywhere.com']
 
 
 # Application definition
