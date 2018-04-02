08ee33e926f26002be8ac5a2b66d331ea6479933
Caleb Myers <cm346613@ohio.edu>
2018-04-02 01:46:00 -0400

    fixed errors in forms, added movie criteria page

--- Builds/Alpha V0.4 Build/movies/forms.py
+++ Builds/Alpha V0.4 Build/movies/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from .movies_api import *
 
 class MovieCriteriaForm(forms.Form):
 	def __init__(self, *args, **vargs):
-		super(CollegeCriteriaForm,self).__init__(*args,**vargs)
+		super(MovieCriteriaForm,self).__init__(*args,**vargs)
 		for i in range(len(APIT)):
 			field = forms.BooleanField(label=APIT[i]['name'],required=False)
 			self.fields[str(i)] = field
@@ -10,7 +11,7 @@ class MovieCriteriaForm(forms.Form):
 class MovieCriteriaWeightForm(forms.Form):
     def __init__(self, *args, **vargs):
         criteria_list = vargs.pop('criteria_list')
-        super(CollegeCriteriaWeightForm,self).__init__(*args,**vargs)
+        super(MovieCriteriaWeightForm,self).__init__(*args,**vargs)
         for i in range(len(criteria_list)):
                 field = forms.IntegerField(label=criteria_list[i],
                                         widget=forms.HiddenInput(attrs={
@@ -24,7 +25,7 @@ AUTO_CHOICES = [(0, "Higher Values = Better Scores"),(1, "Lower Values = Better 
 class MovieAutoScoreForm(forms.Form):
     def __init__(self, *args, **vargs):
         criteria_list = vargs.pop('criteria_list')
-        super(CollegeAutoScoreForm,self).__init__(*args,**vargs)
+        super(MovieAutoScoreForm,self).__init__(*args,**vargs)
         for i in range(len(criteria_list)):
                 field = forms.IntegerField(label=criteria_list[i]['name'],
                                         widget=forms.Select(attrs={'class': 'form-control'},choices=AUTO_CHOICES))
