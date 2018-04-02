e1aed5edb9d8246d8990ed1add95bd8f73ab796c
Caleb Myers <cm346613@ohio.edu>
2018-04-02 00:44:08 -0400

    added movie forms

--- Builds/Alpha V0.4 Build/movies/forms.py
+++ Builds/Alpha V0.4 Build/movies/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-def MovieCriteriaForm(forms.Form):
+class MovieCriteriaForm(forms.Form):
 	year = institution_level = forms.BooleanField(required=False,label="Year of Release")
 	IMDBscore = institution_level = forms.BooleanField(required=False,label="IMDB Score")
 
-class CollegeCriteriaWeightForm(forms.Form):
+class MovieCriteriaWeightForm(forms.Form):
     def __init__(self, *args, **vargs):
         criteria_list = vargs.pop('criteria_list')
         super(CollegeCriteriaWeightForm,self).__init__(*args,**vargs)
@@ -13,4 +13,16 @@ class CollegeCriteriaWeightForm(forms.Form):
                                         widget=forms.HiddenInput(attrs={
                                             'id':('weight_' + str(i))
                                             }))
+                self.fields[str(i)] = field
+
+
+AUTO_CHOICES = [(0, "Higher Values = Better Scores"),(1, "Lower Values = Better Scores")]
+
+class MovieAutoScoreForm(forms.Form):
+    def __init__(self, *args, **vargs):
+        criteria_list = vargs.pop('criteria_list')
+        super(CollegeAutoScoreForm,self).__init__(*args,**vargs)
+        for i in range(len(criteria_list)):
+                field = forms.IntegerField(label=criteria_list[i]['name'],
+                                        widget=forms.Select(attrs={'class': 'form-control'},choices=AUTO_CHOICES))
                 self.fields[str(i)] = field
